6115137295d5a940027bd3b692459822d4edda08
gu90032701 <gu900327@hotmail.com>
2019-08-04 15:35:40 -0500

    2nd

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/Default";
+
 function App() {
-  return <h1>Hello from 5G store</h1>;
+  return (
+    <h1>
+      Hello from 5G store
+      <Home />
+    </h1>
+  );
 }
 
 export default App;
---
+++ src/pages/SingleProductPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SingleProductPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from about Single product Page</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleProductPage;
